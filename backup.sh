fb5421391c4c16eee3d44eecb35a6e5ae6cd4379
wheatdog <wheatdoge@gmail.com>
2016-06-20 19:48:23 +0800

    clean up format

--- backup.sh
+++ backup.sh
@@ -8,48 +8,49 @@ usage()
 
 # $1: source directory
 # $2: destination directory
+# $3: incremental flag
 backup_one_dir()
 {
-	timestamp=`date +%Y-%m-%d.%H-%M-%S`
-	
+    timestamp=`date +%Y-%m-%d.%H-%M-%S`
+
     if [ $3 -eq 0 ]; then
         sourcebasename=$(basename $1)
-	    backupfilename=$2/$sourcebasename.$timestamp.tar.bz2
-	    cd ${sourcedir%/*}
-	    getfattr -Rd $sourcebasename > $sourcebasename.xattr
-	    getfacl -R $sourcebasename > $sourcebasename.acl
-		tar -jpc -f $backupfilename $sourcebasename $sourcebasename.acl $sourcebasename.xattr
+        backupfilename=$2/$sourcebasename.$timestamp.tar.bz2
+        cd ${sourcedir%/*}
+        getfattr -Rd $sourcebasename > $sourcebasename.xattr
+        getfacl -R $sourcebasename > $sourcebasename.acl
+        tar -jpc -f $backupfilename $sourcebasename $sourcebasename.acl $sourcebasename.xattr
         rm $sourcebasename.acl $sourcebasename.xattr
-	else
+    else
         PRE=`cat lastback`
-		sync_target="$2/$timestamp"
+        sync_target="$2/$timestamp"
         OPTS="-avXA --force --delete --link-dest=$PRE"
-		rsync $OPTS $1 $sync_target
+        rsync $OPTS $1 $sync_target
         echo $sync_target > lastback
-	fi
+	  fi
 }
 
 main()
 {
-	[ $# -eq 0 ] && usage
-	local tmp_getopts=`getopt -o hf:s:d:i -- "$@"`
-	eval set -- "$tmp_getopts"
+    [ $# -eq 0 ] && usage
+    local tmp_getopts=`getopt -o hf:s:d:i -- "$@"`
+    eval set -- "$tmp_getopts"
 
     #echo "parameter $@"
-	local file sourcedir destdir
-	local has_config_file=0
-	local incremental=0
-	while true; do
-		case "$1" in
-			-h) usage;;
-			-f) has_config_file=1; file=$2; shift 2;;
-			-s) sourcedir=${2%/}; shift 2;;
-			-d) destdir=${2%/}; shift 2;;
-			-i) incremental=1; shift 1;;
-			--) shift; break;;
-			*) usage;;
-		esac
-	done
+    local file sourcedir destdir
+    local has_config_file=0
+    local incremental=0
+    while true; do
+        case "$1" in
+            -h) usage;;
+            -f) has_config_file=1; file=$2; shift 2;;
+            -s) sourcedir=${2%/}; shift 2;;
+            -d) destdir=${2%/}; shift 2;;
+            -i) incremental=1; shift 1;;
+            --) shift; break;;
+            *) usage;;
+        esac
+    done
 
     #deal with relative path
     if [ "${sourcedir:0:1}" != "/" ]; then
@@ -60,11 +61,11 @@ main()
     fi
 
 
-	if [ "$has_config_file" -eq 1 ]; then
-		echo "Not support config file now!"
-	else
-		backup_one_dir $sourcedir $destdir $incremental
-	fi
+    if [ "$has_config_file" -eq 1 ]; then
+        echo "Not support config file now!"
+    else
+		    backup_one_dir $sourcedir $destdir $incremental
+	  fi
 }
 
 main "$@"
